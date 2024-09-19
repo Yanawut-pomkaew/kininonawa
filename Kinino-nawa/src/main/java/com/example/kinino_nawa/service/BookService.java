803832fe17cce6e6b8be4c7127fbf3598923b3a1
LAPTOP-69759EIU\Acer <yanawut-pom@gmail.com>
2024-09-19 23:07:41 +0700

    Merge branch 'develop' of https://github.com/Yanawut-pomkaew/kininonawa into develop # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- Kinino-nawa/src/main/java/com/example/kinino_nawa/service/BookService.java
+++ Kinino-nawa/src/main/java/com/example/kinino_nawa/service/BookService.java
@@ -12,7 +12,7 @@ public class BookService {
     public BookBean getBookAll(BookBean req) {
         BookBean bean = new BookBean();
         bean.setBookName(req.getBookName());
-        bean.setAuthor("ICW");
+        bean.setAuthor("ICW2");
         return bean;
     }
 }
